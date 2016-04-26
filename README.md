33c0f85aa5b3936d599119bad71e67731af35b7b
Nathan <ul7290@gmail.com>
2016-04-27 01:16:38 +1000

    tidy folder

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # gumtree-topads-viewer
 
 This is a minimal application based on the [Electron](http://electron.atom.io).
+
+# deployment
+
+[electron-packager](https://github.com/electron-userland/electron-packager)
---
+++ src/script.js
@@ -0,0 +1,34 @@
+const gumtreeUrl = "http://www.gumtree.com.au/s-vic/pos+system/k0l3008844";
+const reqBtn = $('#search-button');
+  
+var htmlBody, topadsArea, childrenLocationNode;
+var childrenNode, titles=[], locations=[];
+
+reqBtn.on('click', function() {
+
+	$.ajax({
+  		url: gumtreeUrl,
+	}).done(function(data) {
+  	
+      	htmlBody = $.parseHTML(data);
+      	topadsArea = $(htmlBody).find('#srchrslt-adtable-topads');
+      	childrenNode = $(topadsArea).children();
+
+  		$(childrenNode).find("a span[itemprop='name']").each(function( n,o){
+        	titles.push($.trim($(o).text()));
+	  	});
+  		
+  		$(childrenNode).find(".rs-ad-field .rs-ad-location").each(function(n,o) {
+    		locations.push($(o).text());
+  		});
+  
+  		var html = $("<ul></ul>");
+
+		$.each(titles, function(n) {
+			$(html).append($("<li>" + titles[n] + "," + locations[n] + "</li>"));
+		});
+
+		$('#result').append(html);
+
+	});
+});
